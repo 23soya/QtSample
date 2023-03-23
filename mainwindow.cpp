d4b7bebf0b0678da07cb3f5ac2f558c340fb93b8
Soya Tsunekawa <soyaneco0203@icloud.com>
2023-03-23 16:29:16 +0900

    正解、不正解の文字列追加

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,9 @@ MainWindow::MainWindow(QWidget *parent) :
   ui->graphicsView->setScene(applescene);
   QPixmap pixmap(":/res/fruit_ringo.png");
   applescene->addPixmap(pixmap);
+
+  // 正解 不正解文字列の初期化(nullにする)
+  ui->labeljudge->setText("");
 }
 
 MainWindow::~MainWindow()
@@ -25,14 +28,23 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_Melon_clicked()
 {
   qDebug() << "melon clicked()!";
+
+  // 間違い表示
+  ui->labeljudge->setText("Wrong");
 }
 
 void MainWindow::on_pushButton_Apple_clicked()
 {
   qDebug() << "apple clicked()!";
+
+  // 正解表示
+  ui->labeljudge->setText("Correct!");
 }
 
 void MainWindow::on_pushButton_Strawbery_clicked()
 {
   qDebug() << "strawbery clicked()!";
+
+  // 間違い表示
+  ui->labeljudge->setText("Wrong");
 }
